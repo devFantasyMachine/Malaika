31671ae059b24434423dc1bcab343af390fc3fe6
FantasyMachine <devfantasymachine@gmail.com>
2022-05-03 13:12:15 +0100

    Initial commit

---
+++ social-service/src/main/java/com/enspy/malaika/social/SocialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.enspy.malaika.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialServiceApplication.class, args);
+	}
+
+}
