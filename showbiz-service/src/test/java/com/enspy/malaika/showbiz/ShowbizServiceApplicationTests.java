0874cd77203634db56a9c9b2ce46d3e284cabcbf
FantasyMachine <devfantasymachine@gmail.com>
2022-05-05 19:25:28 +0100

    Initial Commit For Showbiz

---
+++ showbiz-service/src/test/java/com/enspy/malaika/showbiz/ShowbizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enspy.malaika.showbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowbizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
